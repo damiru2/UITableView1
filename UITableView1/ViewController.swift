b3e3d7a4e2e4b96ce04ec03fcc39514ec6cf34c8
damiru2 <chalkarov11@mail.ru>
2022-08-03 21:03:08 +0500

    Initial Commit

---
+++ UITableView1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UITableView1
+//
+//  Created by Damir Chalkarov on 03.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
